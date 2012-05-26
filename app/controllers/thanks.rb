f01dd9faf9e0fe85cf2ae66ee59f5ee38b282a72
Stef Lewandowski <stef@stef.io>
2012-05-26 18:27:30 +0100

    over capacity

--- app/controllers/thanks.rb
+++ app/controllers/thanks.rb
@@ -1,7 +1,11 @@
 Sketching.controllers :thanks do
 
   get :index, :map => "/thanks" do
-    @members = Twitter.list_members("stef", "sketching-with-code")
+    begin
+      @members = Twitter.list_members("stef", "sketching-with-code")
+    rescue Twitter::Error::ServiceUnavailable
+      @message = "Twitter is over capacity!"
+    end
     render :"thanks/index"
   end
   
