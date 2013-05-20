01d8ece6af49195442e6e6f4809730eab3a5a885
Stef Lewandowski <stef@stef.io>
2013-05-20 15:02:45 +0100

    long expire and grid

--- app/controllers/screengrabs.rb
+++ app/controllers/screengrabs.rb
@@ -1,7 +1,7 @@
 Sketching.controllers :screengrabs do
 
   get :show, :map => "/screengrabs/:size/:slug", :provides => [:jpg], :cache => true do
-    expires_in 7200 - rand(500).to_i
+    expires_in 720000 - rand(10000).to_i
     not_found unless screengrab = Screengrab.where(:slug => params[:slug]).first
     content_type "image/jpeg"
     screengrab.thumbnail(params[:size])
